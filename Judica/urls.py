1f573615c3bd7209285b5a071c5296bbd2c54437
Adhivp <adhivp910@gmail.com>
2024-05-20 21:21:32 +0530

    Home/Landing page ready

--- Judica/urls.py
+++ Judica/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from Judica.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',home,name='home')
 ]
