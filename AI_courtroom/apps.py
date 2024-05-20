76fdddc112e028ac3ea8685a062e79a94259fbec
Adhivp <adhivp910@gmail.com>
2024-05-21 04:18:42 +0530

    AI_courtroom app created

---
+++ AI_courtroom/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AiCourtroomConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'AI_courtroom'
---
+++ AI_courtroom/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+# Create your views here.
+def AI_courtroom(request):
+    return render(request,'AI_courtroom.html')
